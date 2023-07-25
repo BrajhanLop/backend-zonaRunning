296d196c8c6dc9d06ef62bda8e8e8536e6bde21d
BrajhanLop <bralopezsuasnabar@gmail.com>
2023-07-25 10:04:43 -0500

    add cita

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,13 +3,17 @@ import { routerUser } from './user.router';
 import { routerService } from './service.router';
 import { routerProfessionals } from './professionals.router';
 import { routerAvatar } from './avatar.router';
+import { routerClients } from './client.router';
+import { citaRouter } from './cita.router';
 
 const globalRouter = Router() ;
 
 //rutas
 globalRouter.use('/professionals', routerProfessionals)
+globalRouter.use('/clients', routerClients )
 globalRouter.use("/users", routerUser)
 globalRouter.use("/services", routerService)
+globalRouter.use("/citas", citaRouter)
 globalRouter.use('/avatars', routerAvatar)
 
 
