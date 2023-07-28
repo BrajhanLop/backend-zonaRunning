07ffe1fdfa0fba9b4bf84006ea760fc19cffc025
BrajhanLop <bralopezsuasnabar@gmail.com>
2023-07-28 17:09:37 -0500

    add fx

--- build/src/models/professionals.js
+++ build/src/models/professionals.js
@@ -4,8 +4,11 @@ const mongoose_1 = require("mongoose");
 const professionSchema = new mongoose_1.Schema({
     especialidad: { type: String, required: true },
     rating: { type: Number, required: true },
+    titulo: { type: String, required: true },
     descripcion: { type: String, required: true },
-    user: { type: mongoose_1.Schema.Types.ObjectId, ref: 'User', required: true }
+    user: { type: mongoose_1.Schema.Types.ObjectId, ref: 'User', required: true },
+    duracion: { type: String, required: true },
+    disponibilidad: { type: mongoose_1.Schema.Types.ObjectId, ref: 'Disponibilidad' }
 });
 const ProfessionModel = (0, mongoose_1.model)('Profession', professionSchema);
 exports.default = ProfessionModel;
