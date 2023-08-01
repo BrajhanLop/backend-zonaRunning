15b4d476e6e089f4ace4beff5d3081111baaada5
Milton David Gago Mercado <miltonmercado92@gmail.com>
2023-08-01 16:11:45 -0600

    authentication with google v0.1

--- build/src/app.js
+++ build/src/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -19,9 +28,10 @@ app.use((0, helmet_1.default)({
 app.use((0, cors_1.default)());
 app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
 app.use('/api/v1', routes_1.default);
-app.get("/", (_req, res) => {
-    return res.send("Welcome to express");
-});
+app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { code } = req.query;
+    res.redirect(`/api/v1/availability/getToken?code=${code}`);
+}));
 //Middlewares despues de las rutas
 app.use(errorHandler_1.default);
 exports.default = app;
