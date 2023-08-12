2c9cf58dd407daf4f4afd9f92fd8f2c1cee3d16e
BrajhanLop <bralopezsuasnabar@gmail.com>
2023-08-12 11:43:35 -0500

    fix conflicts 2

--- src/utils/oAuth2Client.ts
+++ src/utils/oAuth2Client.ts
@@ -1,9 +1,5 @@
 import { google } from "googleapis";
-<<<<<<< HEAD
 const CREDENTIALS_PATH = "../api/credenciales.json"; // Ruta al archivo de credenciales de tu proyecto de Google Cloud
-=======
-const CREDENTIALS_PATH = "../../api/credenciales.json"; // Ruta al archivo de credenciales de tu proyecto de Google Cloud
->>>>>>> deployVercel
 
 const credentials = require(CREDENTIALS_PATH);
 const { client_secret, client_id, redirect_uris } = credentials.installed;
